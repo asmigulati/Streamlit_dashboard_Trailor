ed244e3cdb42c1bfad47d4922dae8fe96bdac3f7
asmigulati <140332527+asmigulati@users.noreply.github.com>
2023-12-15 02:50:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,28 +6,21 @@ from textblob import TextBlob
 import json
 
 # Load data
-@st.cache
+@st.experimental_memo
 def load_data():
     with open('Trailor_feedback.json', 'r') as file:
         data = json.load(file)['feedback_results']
-    return pd.json_normalize(data)
+    df = pd.json_normalize(data)
+    df['itinerary.departure'] = pd.to_datetime(df['itinerary.departure'], format='%d-%m-%y')
+    return df
 
 data = load_data()
-
-# Sentiment analysis on text feedback
-def analyze_sentiment(text):
-    return TextBlob(text).sentiment.polarity if text else 0
-
-data['sentiment'] = data['text_feedback'].apply(analyze_sentiment)
-
-# Convert to datetime
-data['itinerary.departure'] = pd.to_datetime(data['itinerary.departure'], format='%d-%m-%y')
 
 # Prepare the data for visualizations
 def prepare_chart_data(series, top_n=5):
     chart_data = series.value_counts().head(top_n)
     chart_data_percent = (chart_data / chart_data.sum()) * 100
-    return chart_data_percent.reset_index().rename(columns={'index': 'category', 0: 'percent'})
+    return chart_data_percent
 
 # Streamlit layout
 st.title("Itinerary Feedback Analysis Dashboard")
@@ -46,7 +39,7 @@ with st.container():
         st.write(f"{data['itinerary.budget'].mean():.2f}")
 
 with st.expander("Itinerary Details"):
-    origin_filter = st.selectbox("Select Origin", options=['All'] + list(data['itinerary.origin'].unique()))
+    origin_filter = st.selectbox("Select Origin", options=['All'] + sorted(data['itinerary.origin'].unique()))
     if origin_filter != 'All':
         filtered_data = data[data['itinerary.origin'] == origin_filter]
     else:
@@ -60,32 +53,44 @@ with st.container():
     with col1:
         st.subheader("Liked/Disliked Itineraries")
         like_dislike_counts = data['liked'].value_counts(normalize=True) * 100
-        st.bar_chart(like_dislike_counts)
+        like_dislike_data = pd.DataFrame(like_dislike_counts).reset_index()
+        like_dislike_data.columns = ['Response', 'Percentage']
+        st.bar_chart(like_dislike_data.set_index('Response'))
+
+with st.container():
+    col1, col2 = st.columns(2)
+    with col1:
+        st.subheader("Top Common Destinations")
+        top_n_dest = st.slider("Select number of top destinations", 1, 20, 5, key='slider_dest')
+        top_destinations_data = prepare_chart_data(data['itinerary.destination'], top_n=top_n_dest)
+        top_destinations_chart_data = pd.DataFrame(top_destinations_data).reset_index()
+        top_destinations_chart_data.columns = ['Destination', 'Percentage']
+        st.bar_chart(top_destinations_chart_data.set_index('Destination'))
+
     with col2:
-        st.subheader("Sentiment Analysis of Feedback")
-        st.bar_chart(data['sentiment'])
+        st.subheader("Top Common Origins")
+        top_n_orig = st.slider("Select number of top origins", 1, 20, 5, key='slider_orig')
+        top_origins_data = prepare_chart_data(data['itinerary.origin'], top_n=top_n_orig)
+        top_origins_chart_data = pd.DataFrame(top_origins_data).reset_index()
+        top_origins_chart_data.columns = ['Origin', 'Percentage']
+        st.bar_chart(top_origins_chart_data.set_index('Origin'))
 
 with st.container():
-    st.subheader("Top Common Destinations")
-    top_n_dest = st.slider("Select number of top destinations", 1, 20, 5, key='slider_dest')
-    top_destinations_data = prepare_chart_data(data['itinerary.destination'], top_n=top_n_dest)
-    st.bar_chart(top_destinations_data.set_index('category'))
+    col1, col2 = st.columns(2)
+    with col1:
+        st.subheader("Top Vibes")
+        top_n_vibes = st.slider("Select number of top vibes", 1, 20, 5, key='slider_vibes')
+        top_vibes_data = prepare_chart_data(data['itinerary.vibe'], top_n=top_n_vibes)
+        top_vibes_chart_data = pd.DataFrame(top_vibes_data).reset_index()
+        top_vibes_chart_data.columns = ['Vibe', 'Percentage']
+        st.bar_chart(top_vibes_chart_data.set_index('Vibe'))
 
-    st.subheader("Top Common Origins")
-    top_n_orig = st.slider("Select number of top origins", 1, 20, 5, key='slider_orig')
-    top_origins_data = prepare_chart_data(data['itinerary.origin'], top_n=top_n_orig)
-    st.bar_chart(top_origins_data.set_index('category'))
-
-    st.subheader("Top Vibes")
-    top_n_vibes = st.slider("Select number of top vibes", 1, 20, 5, key='slider_vibes')
-    top_vibes_data = prepare_chart_data(data['itinerary.vibe'], top_n=top_n_vibes)
-    st.bar_chart(top_vibes_data.set_index('category'))
+    with col2:
+        st.subheader("Budget Distribution")
+        sns.histplot(data['itinerary.budget'], kde=True)
+        st.pyplot(plt)
 
 with st.container():
-    st.subheader("Budget Distribution")
-    sns.histplot(data['itinerary.budget'], kde=True)
-    st.pyplot(plt)
-
     st.subheader("Itineraries Over Time")
     sns.lineplot(x='itinerary.departure', y='liked', data=data)
     st.pyplot(plt)
